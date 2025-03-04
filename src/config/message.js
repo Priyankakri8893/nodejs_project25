6ffa0cc9cb2cebc219191abf1d17305c6962f3fb
priyanka <priyankakri8893@gmail.com>
2025-03-04 09:11:22 +0530

    msg

--- src/config/message.js
+++ src/config/message.js
@@ -1,21 +1,36 @@
-const msg = {
-  success: "Ok",
-  invalidPassword: "Password is invalid",
-  userNotFound: "User not found",
-  password_updated: "Password is successfully updated",
-  profile_updated: "Profile is successfully updated",
-  loggedIn: "Successfully logIn",
-  expireOtp: "Otp is expired",
-  incorrectOTP: "Incorrect Otp",
-  duplicateEmail: "Email already exists",
-  duplicateEmailOrPhone: "User already exists",
-  invalidEmail: "Email address is invalid",
-  invalidPhone: "Phone number is invalid",
-  NotExist: "Do not exist",
-  NotCreated: "Do not created, try again",
-  notSaved: "Not saved",
-  unauthorisedRequest: 'Unauthorised request',
-  actionForbidden: "This action is forbidden. You don't have permission to do this",
-};
+class MessageService {
+  constructor() {
+    this.messages = {
+      success: "Ok",
+      invalidPassword: "Password is invalid",
+      userNotFound: "User not found",
+      password_updated: "Password is successfully updated",
+      profile_updated: "Profile is successfully updated",
+      loggedIn: "Successfully logIn",
+      expireOtp: "Otp is expired",
+      incorrectOTP: "Incorrect Otp",
+      duplicateEmail: "Email already exists",
+      duplicateEmailOrPhone: "User already exists",
+      invalidEmail: "Email address is invalid",
+      invalidPhone: "Phone number is invalid",
+      NotExist: "Do not exist",
+      NotCreated: "Do not created, try again",
+      notSaved: "Not saved",
+      unauthorisedRequest: "Unauthorised request",
+      actionForbidden: "This action is forbidden. You don't have permission to do this"
+    };
+
+    return new Proxy(this, {
+      get: (target, prop) => {
+        if (prop in target.messages) {
+          return target.messages[prop];
+        }
+        return `Message for ${prop} not found`;
+      }
+    });
+  }
+}
+
+const msg = new MessageService();
 
 module.exports = { msg };
